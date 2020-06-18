585d305b2de35f17dc26a6c0806ac6692189a277
hoan242 <hoann242@gmail.com>
2020-06-18 09:15:48 +0700

    Update Kcar.js

--- Kcar.js
+++ Kcar.js
@@ -1,47 +1,3 @@
-var shift = false;
-var ctrl = false;
-var alt = false;
-var space = false;
-$(document).on("keydown", function (event) {
-    if (event.which == 16) {
-        shift = true;
-    } else if (event.which == 17) {
-        ctrl = true;
-    } else if (event.which == 18) {
-        alt = true;
-    } else if (event.which == 32) {
-        undo(selectedPointList, lastSelectedGroupId);
-    } else if (event.which == 192) {    
-        addNewObject('polygon');
-    }
-}).on("keyup", function (event) {
-    shift = false;
-    ctrl = false;
-    alt = false;
-});
-$(document).bind('mousewheel DOMMouseScroll', function (event) {
-      if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-          if (ctrl == false && alt == false) {
-              //actionWheel(data_parr, false, shift);
-          }
-          return false;
-      } else {
-          if (ctrl == false && alt == false) {
-            // actionWheel(data_parr, true, shift);
-          }
-          return false;
-      }
-  });
-$(document).bind('mousewheel DOMMouseScroll', function (event) {
-		if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-			zoomIn();
-			return false;
-		} else {
-			zoomOut();
-			return false;
-		}
-	});
-
 function ClonePolygon(draws_x, draws_y, annot, attr) {
 
     var draw_x = draws_x;
@@ -1333,7 +1289,7 @@ $(".task-title").html(contentImg + '<a href="http://dcamp.sweetx.co.kr/labelme/I
 //function paste() {
 document.addEventListener('paste', function (event) {
     var clipText = event.clipboardData.getData('Text');
-//        console.log(clipText);
+        console.log("Paste::" + clipText);
     AddPolygon(clipText);
 });
 
